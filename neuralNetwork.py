12db5689697c9d2a91a1594dda55c1b64d8d9f20
Rudolf Nosek <rudolfnosek.rn@gmail.com>
2021-12-26 23:22:22 +0100

    proposed structure of NeuralNetwork

---
+++ neuralNetwork.py
@@ -0,0 +1,11 @@
+
+class NeuralNetwork:
+
+    def _init__(self, num_layers:int, num_perceptrons:list):
+        pass
+    
+    def fit(self, X_train, y_train, X_test, y_test):
+        pass
+
+    def predict(X):
+        pass
