01e1185f7679f6cb30e6ace92a7b8d33cd009730
Rudolf Nosek <rudolfnosek.rn@gmail.com>
2022-01-08 11:07:55 +0100

    Training module created

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -3,10 +3,12 @@ import numpy as np
 class NeuralNetwork:
 
     def __init__(self, num_layers:int, num_perceptrons:list, alpha:float = 0.1):
+        if num_layers != len(num_perceptrons):
+            raise AttributeError('Number of layers and perceptron list size does not match')
         self.W_list = []
         self.num_layers = num_layers
         self.num_n = num_perceptrons
-        self.apha = alpha
+        self.alpha = alpha
         self._create_weights()
         
     def _create_weights(self):
@@ -24,17 +26,17 @@ class NeuralNetwork:
 
     def sigmoid(self, x:float or np.array)->float or np.array:
         '''Activation function - logistical sigmoid'''
-        if isinstance(x, np.ndarray):
-            return np.array([self.sigmoid(i) for i in x])
         return 1/(1 + np.exp(-x))
-    
+
+    def sigmoid_derivation(self,x:float or np.array)->float or np.array:
+        return self.sigmoid(x)*(1-self.sigmoid(x))
+
     def relu(self,x:float or np.array)->float or np.array:
         '''Activation function - Rectified Linear Unit'''
         if isinstance(x, np.ndarray):
             return np.array([self.relu(i) for i in x])
         return max([0,x])
 
-        return max([0,x])
     def relu_derivation(self,x:float or np.array)->int or np.array:
         ''' derivation of relu activation function
             raises:
@@ -49,7 +51,10 @@ class NeuralNetwork:
 
     def calculate_error(self,predicted:float,real:float)->float:
         '''method for calculating error'''
-        return (predicted-real)**2
+        return ((predicted-real)**2)/2
+
+    def calculate_accuracy(self, inputs, targets):
+        pass
 
     def fit(self, X_train, y_train, num_epochs=10):
         
@@ -59,26 +64,47 @@ class NeuralNetwork:
         for ep in range(num_epochs):
             E = 0
 
-            for i in np.random.permutation(X_train.shapep[0]):
-                x = self._add_bias(X_train[i,:]) 
+            for per in np.random.permutation(X_train.shape[0]):
+                x = self._add_bias(X_train[per,:]) 
 
-                for i in range(self.num_layers-1):
-                    X = np.dot(X,self.W_list[i])
-                    X = np.array([self.relu(row) for row in X]) # applying activation function
+                net = [x] # outputs of the network without activation function
+                deltas = [] # computed errors of neurons
+                inputs = [x]
 
-                y = y_train[i,:]
-                E += self.calculate_error(predicted,y)
+                for i in range(self.num_layers-1):              # calculating output from NN
+                    x = np.dot(x,self.W_list[i])                # multiplying with weights
+                    net.append(x)                         # storing output for error calculation
+                    x = np.array([self.sigmoid(row) for row in x]) # applying activation function
+                    inputs.append(x)
 
-                #for j in range(self.num_layers-1):
+                y = y_train[per]
+                E += self.calculate_error(x[0],y)
+
+                for i in range(self.num_layers-1,-1,-1): # computing derivation of error for every layer
+                    if i == self.num_layers-1: #if output layer
+                        d = (x-y) * net[-1]
+                    elif i == 0:                        # if input layer
+                        d = np.dot(self.W_list[i],deltas[-1]) 
+                    else:
+                        d = np.dot(self.W_list[i],deltas[-1]) * self.sigmoid_derivation(net[-1])
+
+                    deltas.append(d)
+                    net.pop()
+                deltas.reverse()
+                for i in range(self.num_layers-1): # updating weights
+                    self.W_list[i] += (self.alpha * np.dot( deltas[i] ,inputs[i]))
+            error_history.append(E/X_train.shape[0])
+            #acc = self.compute_accuracy(inputs, targets)
+            #accuracy_history.append(acc)
+            if (ep+1) % 10 == 0: print(f'Epoch {ep+1}, E = {error_history[-1]}, accuracy = {0}')
+
+        return error_history,accuracy_history
 
     def predict(self,X:np.array)->np.array:
         '''for given input return prediction'''
         X = np.array([ self._add_bias(x) for x in X])
         for i in range(self.num_layers-1):
             X = np.dot(X,self.W_list[i])
-            X = np.array([self.relu(row) for row in X]) # applying activation function
+            X = np.array([self.sigmoid(row) for row in X]) # applying activation function
         return X
 
-model = NeuralNetwork(4,[2,5,3,1])
-data = np.array([[i,i] for i in range(10)])
-print(model.predict(data))
