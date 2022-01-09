5607c198a7e0b2474c360e66f2d5da705841a66d
Rudolf Nosek <rudolfnosek.rn@gmail.com>
2022-01-09 16:41:54 +0100

    changed error calculation

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class NeuralNetwork:
 
-    def __init__(self, num_perceptrons:list):
+    def __init__(self, num_perceptrons:list,activation:str):
         num_layers = 3
         if num_layers != len(num_perceptrons):
             raise AttributeError('Number of layers and perceptron list size does not match')
@@ -10,6 +10,19 @@ class NeuralNetwork:
         self.num_layers = num_layers
         self.num_n = num_perceptrons
         self._create_weights()
+
+        if activation in ['tanh','relu','sigmoid']:
+            if activation == 'sigmoid':
+                self.fun = self.sigmoid
+                self.der_fun = self.sigmoid_derivation
+            elif activation == 'relu':
+                self.fun = self.relu
+                self.der_fun = self.relu_derivation
+            elif activation == 'tanh':
+                self.fun = self.tanh
+                self.der_fun = self.tanh_derivation
+        else:
+            raise AttributeError('Unknown activation function')
         
     def _create_weights(self):
         '''random initialization of weights'''
@@ -59,20 +72,16 @@ class NeuralNetwork:
             return 0
         raise AttributeError('for 0 derivation of relu is not defined')
 
-    def calculate_error(self,predicted:float,real:float)->float:
+    def calculate_error(self,predicted:np.array,real:np.array)->float:
         '''method for calculating error'''
-        return ((real-predicted)**2)/2
+        return np.mean((real-predicted)**2)
 
-    def calculate_accuracy(self, inputs, targets):
-        pass
-
-    def fit(self, X_train, y_train, num_epochs=10, alpha:float = 0.1):
+    def fit(self, X_train, y_train, X_test,y_test, num_epochs=10, alpha:float = 0.1):
         
         error_history = []
-        accuracy_history = []
+        test_error_history = []
 
         for ep in range(num_epochs):
-            E = 0
 
             for per in np.random.permutation(X_train.shape[0]):
                 x = self._add_bias(X_train[per,:]) 
@@ -80,32 +89,34 @@ class NeuralNetwork:
                 net = [0,0]
 
                 net[0] = np.dot(x,self.W_list[0])
-                h  = self.sigmoid(net[0])
+                h  = self.fun(net[0])
 
                 net[1] = np.dot(h,self.W_list[1])
                 y = net[1][0] # from array to numbers
 
                 d = y_train[per]
-                E += self.calculate_error(predicted=y,real=d)
                 
                 deltas =  [0,0]
                 deltas[1] = np.array([d-y])
-                deltas[0] = self.W_list[1].T * deltas[1] * self.sigmoid_derivation(net[0])   
+                deltas[0] = self.W_list[1].T * deltas[1] * self.der_fun(net[0])   
 
                 self.W_list[0] += (alpha * np.outer(x,deltas[0]))
                 self.W_list[1] += (alpha * np.outer(h,deltas[1]))
 
-            error_history.append(E/X_train.shape[0])
-            #acc = self.compute_accuracy(inputs, targets)
-            #accuracy_history.append(acc)
-            if (ep+1) % 10 == 0: print(f'Epoch {ep+1}, E = {error_history[-1]}, accuracy = {0}')
+            predicted = self.predict(X_train)
+            E = self.calculate_error(predicted, y_train)
+            error_history.append(E)
 
-        return error_history,accuracy_history
+            predicted = self.predict(X_test)
+            E = self.calculate_error(predicted, y_test)
+            test_error_history.append(E)
+
+        return error_history,test_error_history
 
     def predict(self,X:np.array)->np.array:
         '''for given input return prediction'''
         X = np.array([ self._add_bias(x) for x in X])
         for i in range(self.num_layers-1):
             X = np.dot(X,self.W_list[i])
-            X = np.array([self.sigmoid(row) for row in X]) # applying activation function
-        return X
+            X = np.array([self.fun(row) for row in X]) # applying activation function
+        return X.reshape(X.shape[0])
